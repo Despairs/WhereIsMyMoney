fcd3278c800312dbc0956464c433e9b906b7e473
Evgeny Kovtunenko <ekovtunenko@iflex.ru>
2019-11-26 15:11:35 +0300

    Очень на коленке сделал детализацию по типам платежей

--- app/src/main/java/org/despairs/wmm/ui/home/HomeFragment.java
+++ app/src/main/java/org/despairs/wmm/ui/home/HomeFragment.java
@@ -13,6 +13,7 @@ import org.despairs.wmm.ui.home.adapter.SalaryAdapter;
 
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -23,12 +24,12 @@ import java.util.stream.Stream;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class HomeFragment extends Fragment {
 
-    private static final Pattern SALARY_SMS_PATTRETN = Pattern.compile(".*\\s(\\d+(\\.\\d+)?)р Баланс.*");
+    private static final Pattern SALARY_SMS_PATTERN = Pattern.compile(".*\\s(\\d+(\\.\\d+)?)р Баланс.*");
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_home, container, false);
@@ -37,8 +38,8 @@ public class HomeFragment extends Fragment {
             Context context = root.getContext();
             RecyclerView recyclerView = (RecyclerView) root;
             recyclerView.setHasFixedSize(true);
-            recyclerView.setLayoutManager(new LinearLayoutManager(context));
             recyclerView.setAdapter(new SalaryAdapter(getSalary()));
+            recyclerView.addItemDecoration(new DividerItemDecoration(context, DividerItemDecoration.VERTICAL));
         }
 
         return root;
@@ -47,26 +48,49 @@ public class HomeFragment extends Fragment {
     private List<Salary> getSalary() {
         List<Sms> messages = new JdbcSmsRepository().listBySenderAndMessageContainingWords("900", "зарплаты", "отпускных", "аванса", "премии");
         //@TODO не использовать Salary как промежуточное хранилище?
-        Map<LocalDateTime, Double> collect = messages.stream()
+        List<Salary> list = messages.stream()
                 .map(this::tryToParseAsSalary)
                 .flatMap(o -> o.map(Stream::of).orElseGet(Stream::empty))
+                .collect(Collectors.toList());
+
+        Map<LocalDateTime, Double> collect = list.stream()
                 .collect(Collectors.groupingBy(Salary::getBillingPeriod, Collectors.summingDouble(Salary::getAmount)));
 
         return collect.entrySet().stream()
-                .map(entry -> new Salary(entry.getKey(), entry.getValue()))
+                .map(entry -> new Salary(entry.getKey(), entry.getValue(), getAllByPeriod(list, entry.getKey())))
                 .sorted((o1, o2) -> o2.getBillingPeriod().compareTo(o1.getBillingPeriod()))
+                .collect(Collectors.toList());
+    }
+
+    private List<Salary> getAllByPeriod(List<Salary> salaries, LocalDateTime billingPeriod) {
+        return salaries.stream()
+                .filter(s -> s.billingPeriod.equals(billingPeriod))
                 .collect(Collectors.toList());
     }
 
     private Optional<Salary> tryToParseAsSalary(Sms sms) {
         Optional<Salary> ret = Optional.empty();
-        Matcher matcher = SALARY_SMS_PATTRETN.matcher(sms.getMessage());
+        Matcher matcher = SALARY_SMS_PATTERN.matcher(sms.getMessage());
         if (matcher.matches()) {
             double amount = Double.parseDouble(matcher.group(1));
             LocalDateTime period = getBillingPeriod(sms);
-            ret = Optional.of(new Salary(period, amount));
+            ret = Optional.of(new Salary(period, amount, getType(sms)));
         }
         return ret;
+    }
+
+    private String getType(Sms sms) {
+        LocalDateTime date = sms.getDate();
+        String message = sms.getMessage();
+        if (message.contains("зарплаты")) {
+            return date.getDayOfMonth() < 20 ? "Остатки" : "Аванс";
+        } else if (message.contains("отпускных")) {
+            return "Отпускные";
+        } else if (message.contains("аванса")) {
+            return "Аванс";
+        } else if (message.contains("премии")) {
+            return "Премия";
+        } else return "Неизвестно";
     }
 
     private LocalDateTime getBillingPeriod(Sms sms) {
@@ -78,14 +102,29 @@ public class HomeFragment extends Fragment {
         return date.withDayOfMonth(1).truncatedTo(ChronoUnit.DAYS);
     }
 
-    public class Salary {
+    public static class Salary {
 
         public double amount;
         public LocalDateTime billingPeriod;
+        public String type;
+        public List<Salary> items = Collections.emptyList();
+        public boolean expanded;
 
         public Salary(LocalDateTime billingPeriod, double amount) {
             this.amount = amount;
             this.billingPeriod = billingPeriod;
+        }
+
+        public Salary(LocalDateTime billingPeriod, double amount, List<Salary> items) {
+            this.amount = amount;
+            this.billingPeriod = billingPeriod;
+            this.items = items;
+        }
+
+        public Salary(LocalDateTime billingPeriod, double amount, String type) {
+            this.amount = amount;
+            this.billingPeriod = billingPeriod;
+            this.type = type;
         }
 
         public double getAmount() {
--- app/src/main/java/org/despairs/wmm/ui/home/adapter/SalaryAdapter.java
+++ app/src/main/java/org/despairs/wmm/ui/home/adapter/SalaryAdapter.java
@@ -3,16 +3,21 @@ package org.despairs.wmm.ui.home.adapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
-import org.despairs.wmm.App;
+import org.despairs.wmm.BR;
 import org.despairs.wmm.R;
+import org.despairs.wmm.databinding.FragmentHomeListItemBinding;
 import org.despairs.wmm.ui.home.HomeFragment;
 
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Locale;
 
 import androidx.annotation.NonNull;
+import androidx.databinding.BindingConversion;
+import androidx.databinding.DataBindingUtil;
+import androidx.databinding.ViewDataBinding;
 import androidx.recyclerview.widget.RecyclerView;
 
 /**
@@ -20,7 +25,8 @@ import androidx.recyclerview.widget.RecyclerView;
  */
 public class SalaryAdapter extends RecyclerView.Adapter<SalaryAdapter.ViewHolder> {
 
-    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("LLLL YYYY");
+    private static final Locale RU = Locale.forLanguageTag("ru");
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("LLLL YYYY", RU);
     private List<HomeFragment.Salary> items;
 
     public SalaryAdapter(List<HomeFragment.Salary> items) {
@@ -30,16 +36,25 @@ public class SalaryAdapter extends RecyclerView.Adapter<SalaryAdapter.ViewHolder
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.fragment_home_list_item, parent, false);
-        return new ViewHolder(v);
+        FragmentHomeListItemBinding viewBinding = DataBindingUtil.inflate(LayoutInflater.from(parent.getContext()), R.layout.fragment_home_list_item, parent, false);
+        return new ViewHolder(viewBinding);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         HomeFragment.Salary salary = items.get(position);
 
-        holder.factAmountView.setText(App.context.getString(R.string.salary_amount_fact, salary.getAmount()));
-        holder.periodView.setText(FORMATTER.format(salary.getBillingPeriod()));
+        holder.binding.detailList.setHasFixedSize(true);
+        holder.binding.detailList.setAdapter(new DetailedSalaryAdapter(salary.items));
+        holder.binding.detailList.setVisibility(salary.expanded ? View.VISIBLE : View.GONE);
+        holder.binding.setVariable(BR.salary, salary);
+        holder.binding.executePendingBindings();
+
+        holder.binding.fullItem.setOnClickListener(v -> {
+            salary.expanded = !salary.expanded;
+            notifyItemChanged(position);
+        });
+
     }
 
     @Override
@@ -49,14 +64,63 @@ public class SalaryAdapter extends RecyclerView.Adapter<SalaryAdapter.ViewHolder
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
 
-        private TextView factAmountView;
-        private TextView periodView;
+        private FragmentHomeListItemBinding binding;
 
-        ViewHolder(View v) {
-            super(v);
-            factAmountView = v.findViewById(R.id.fragment_home_list_item_text_view_fact_amount);
-            periodView = v.findViewById(R.id.fragment_home_list_item_text_view_period);
+        ViewHolder(FragmentHomeListItemBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
         }
 
     }
+
+    @BindingConversion
+    public static String convertBillingPeriodToString(LocalDateTime period) {
+        return FORMATTER.format(period);
+    }
+
+    @BindingConversion
+    public static String convertAmountToString(double period) {
+        return String.format(RU, "%1$,.2f \u20BD", period);
+    }
+
+    public static class DetailedSalaryAdapter extends RecyclerView.Adapter<DetailViewHolder> {
+
+        private List<HomeFragment.Salary> items;
+
+        public DetailedSalaryAdapter(List<HomeFragment.Salary> items) {
+            this.items = items;
+        }
+
+        @NonNull
+        @Override
+        public DetailViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            ViewDataBinding binding = DataBindingUtil.inflate(LayoutInflater.from(parent.getContext()), R.layout.fragment_home_list_item_detail_item, parent, false);
+            return new DetailViewHolder(binding);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull DetailViewHolder holder, int position) {
+            HomeFragment.Salary salary = items.get(position);
+
+            holder.binding.setVariable(BR.salary, salary);
+            holder.binding.executePendingBindings();
+        }
+
+        @Override
+        public int getItemCount() {
+            return items.size();
+        }
+    }
+
+    public static class DetailViewHolder extends RecyclerView.ViewHolder {
+
+        private ViewDataBinding binding;
+
+        DetailViewHolder(ViewDataBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+        }
+
+    }
+
 }
